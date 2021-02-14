ea0527af2b45ea16f09581765ebb28f2998aa19e
arnab <arnab>
2021-02-14 14:40:35 +0530

    updated code

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = function mergeClassNames(...classes) {
+  let str = '';
+
+  if (classes && classes.length > 0) {
+    for (let index = 0; index < classes.length; index++) {
+      const item = classes[index];
+      if (typeof item === 'string') {
+        str = str + ' ' + item;
+      } else if (typeof item === 'object') {
+        for (const key in item) {
+          if (Object.prototype.hasOwnProperty.call(item, key)) {
+            const value = item[key];
+            if (value === true) {
+              str = str + ' ' + key;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return str;
+};
